ccff44b735de27542d1be34fddea2906a02be80a
Joshua Zon <carlos@Joshuas-MacBook-Air.local>
2026-03-09 20:59:01 +0700

    point to railway api

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000';
+const API_URL = 'https://ai-visibility-api-production.up.railway.app';
 
 const api = axios.create({ baseURL: API_URL });
 
